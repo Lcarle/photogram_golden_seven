078bac64d322f07ec20c38cbe7ce9358157671b9
Lcarle <lcarle@users.noreply.github.com>
2016-10-30 19:27:17 -0500

    draft

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,20 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @photo = Photo.find_by({ :id => params[:id]})
+    @photo_source = @photo.source
+    @photo_caption = @photo.caption
+    @photo_id = @photo.id
+    @photo_id = @photo.id
+
+  end
+
+  def new_form
+
+  end
+
+  def create_row
+  end
 
 end
